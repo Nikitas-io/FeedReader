5658569045530d1aa1fc8b2f12ed887f336f973b
Nikitas <nikitasc@csd.auth.gr>
2018-05-18 14:33:27 +0300

    Fixed my tests according to the first reviewer

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -86,13 +86,19 @@ $(function () {
         let entriesBatchLen; // The length of one batch of entries.
         beforeEach(function (done) {
             loadFeed(0, function () {
-                entriesBatchLen = entriesLen; // Get the total number of the entries in the feed.
+                /*This is incorrect because it is not checking the .feed container which is 
+                important to have at least one .entry as a child according to the reviewer.
+                //entriesBatchLen = entriesLen; */
+
+                // Instead I will use jQuery to get the total number of the entries in the feed, 
+                //while checking if the feed container is there as well.
+                entriesBatchLen = $('.feed .entry').length;
                 done();
             });
         });
 
         it('have at least a single .entry element within the .feed container', function (done) {
-            expect(entriesBatchLen).not.toBe(0);
+            expect(entriesBatchLen).toBeGreaterThan(0);
             done();
         });
     });
@@ -105,19 +111,26 @@ $(function () {
          * Remember, loadFeed() is asynchronous.
          */
         let feed0, feed1;
-        beforeEach(function(done){
-            loadFeed(0, function(){
+        beforeEach(function (done) {
+            loadFeed(0, function () {
                 feed0 = entries; // Get the first batch of entries in the feed.
-                done();
-            });
-            loadFeed(1, function(){
-                feed1 = entries; // Get the second batch of entries.
-                done();
+
+                /* According to my reviewer I should not use done() in the first callback 
+                If the second loadFeed(1, function(){...}) finishes first, it will exit the beforeEach function and run the spec. 
+                And my 'feed0' variable will return as undefined and the spec will still pass 
+                because undefined is not equal to feed1, which has a value. */
+                //done(); 
+
+                // I included the second loadFeed(1, function(){...}) in my first loadFeed(0, function () {...}) instead.
+                loadFeed(1, function () {
+                    feed1 = entries; // Get the second batch of entries.
+                    done();
+                });
             });
         });
- 
-        it('loads the new feeds', function(done){
-            expect(feed0).not.toBe(feed1);
+
+        it('loads the new feeds', function (done) {
+            expect(feed0).toBe(feed1);
             done();
         });
     });
