ceb97937767d7ff8bc72f2831e455da3a04c7e36
Nikitas <nikitasc@csd.auth.gr>
2018-05-18 03:05:21 +0300

    Made the comments nice

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,7 +1,7 @@
 /* feedreader.js
  *
  * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
+ * all of the tests that will be run against the application.
  */
 
 /* We're placing all of our tests within the $() function,
@@ -9,17 +9,10 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function () {
-    /* This is our first test suite - a test suite just contains
-     * a related set of tests. This suite is all about the RSS
-     * feeds definitions, the allFeeds variable in our application.
-     */
     describe('RSS Feeds', function () {
-        /* This is our first test - it tests to make sure that the
+        /* This test makes sure that the
          * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+         * empty. 
          */
 
         it('are defined', function () {
@@ -28,7 +21,8 @@ $(function () {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* 
+         * A test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -39,7 +33,8 @@ $(function () {
             });
         });
 
-        /* TODO: Write a test that loops through each feed
+        /* 
+         * A test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -52,21 +47,20 @@ $(function () {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function () {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /*
+         * A test that ensures that the menu element is
+         * hidden by default. 
          */
         it('is hidden by default', function () {
             expect(document.body.classList.contains('menu-hidden')).toBe(true);
         });
 
-        /* TODO: Write a test that ensures the menu changes
+        /*
+         * A test that ensures that the menu changes
          * visibility when the menu icon is clicked. This test
-         * should have two expectations: does the menu display when
+         * has two expectations: does the menu display when
          * clicked and does it hide when clicked again.
          */
         it('changes visibility when its icon is clicked', function () {
